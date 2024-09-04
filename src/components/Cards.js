13d7ab0558f0c7676ed7c8bef8b67ea38c517476
nerellamanoj <nerellamanojkumar@gmail.com>
2024-09-05 00:52:07 +0530

    add space

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Cardsdata from './CardsData';
-import './style.css';
-import { useDispatch } from 'react-redux';
-import { ADD } from '../redux/actions/action';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Cardsdata from "./CardsData";
+import "./style.css";
+import { useDispatch } from "react-redux";
+import { ADD } from "../redux/actions/action";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Cards = () => {
-
   const [data, setData] = useState(Cardsdata);
   const dispatch = useDispatch();
 
@@ -28,20 +27,31 @@ const Cards = () => {
   };
 
   return (
-    <div className='container mt-3'>
-      <h2 className='text-center'>Add to Cart Projects</h2>
+    <div className="container mt-3">
+      <h2 className="text-center">Add to Cart Projects</h2>
 
       <div className="row d-flex justify-content-center align-items-center">
         {data.map((element, id) => (
-          <Card key={id} style={{ width: '22rem', border: 'none' }} className="mx-2 mt-4 card_style">
-            <Card.Img variant="top" src={element.imgdata} style={{ height: '16rem' }} className="mt-3" />
+          <Card
+            key={id}
+            style={{ width: "22rem", border: "none" }}
+            className="mx-2 mt-4 card_style"
+          >
+            <Card.Img
+              variant="top"
+              src={element.imgdata}
+              style={{ height: "16rem" }}
+              className="mt-3"
+            />
             <Card.Body>
               <Card.Title>{element.rname}</Card.Title>
-              <Card.Text>
-                Price: ₹ {element.price}
-              </Card.Text>
+              <Card.Text>Price: ₹ {element.price}</Card.Text>
               <div className="button_div d-flex justify-content-center">
-                <Button variant="primary" onClick={() => send(element)} className='w-100'>
+                <Button
+                  variant="primary"
+                  onClick={() => send(element)}
+                  className="w-100"
+                >
                   Add to Cart
                 </Button>
               </div>
@@ -54,6 +64,6 @@ const Cards = () => {
       <ToastContainer />
     </div>
   );
-}
+};
 
 export default Cards;
